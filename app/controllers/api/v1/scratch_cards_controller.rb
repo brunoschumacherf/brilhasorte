efd55a63e622af5dfcf9ff1f56a32afdafaaa2c6
Bruno Schumacher <brunoschumacherfsouza@gmail.com>
2025-08-04 02:20:01 -0300

    feat: fix controller scratch

--- app/controllers/api/v1/scratch_cards_controller.rb
+++ app/controllers/api/v1/scratch_cards_controller.rb
@@ -1,15 +1,8 @@
-class Api::V1::GamesController < ApplicationController
+class Api::V1::ScratchCardsController < ApplicationController
   before_action :authenticate_user!
 
-  def create
-    scratch_card = ScratchCard.find(params[:scratch_card_id])
-    result = GameCreationService.new(current_user, scratch_card).call
-
-    if result.success?
-      # Criamos um Serializer para não expor dados sensíveis como o prêmio ou o seed.
-      render json: GameSerializer.new(result.game).serializable_hash, status: :created
-    else
-      render json: { error: result.error_message }, status: :unprocessable_entity
-    end
+  def index
+    @scratch_cards = ScratchCard.where(is_active: true).order(:price_in_cents)
+    render json: ScratchCardSerializer.new(@scratch_cards, include: [:prizes]).serializable_hash, status: :ok
   end
 end
