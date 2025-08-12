89aa26a5236984646334e20404e3a5813ee9e4f0
Bruno Schumacher <brunoschumacherfsouza@gmail.com>
2025-08-12 02:24:57 -0300

    fix list scratch

--- app/serializers/deposit_serializer.rb
+++ app/serializers/deposit_serializer.rb
@@ -3,12 +3,20 @@ class DepositSerializer
   attributes :id, :amount_in_cents, :status, :created_at
 
   attribute :pix_qr_code_payload do |deposit, params|
-    params[:gateway_response].dig(:response, "point_of_interaction", "transaction_data", "qr_code")
+    if params[:gateway_response].present?
+      params[:gateway_response].dig(:response, "point_of_interaction", "transaction_data", "qr_code")
+    else
+        ''
+    end
   end
 
   attribute :pix_qr_code_image_base64 do |deposit, params|
-    base64_data = params[:gateway_response].dig(:response, "point_of_interaction", "transaction_data", "qr_code_base64")
+    if params[:gateway_response].present?
+      base64_data = params[:gateway_response].dig(:response, "point_of_interaction", "transaction_data", "qr_code_base64")
 
-    "data:image/png;base64,#{base64_data}" if base64_data.present?
+      "data:image/png;base64,#{base64_data}" if base64_data.present?
+    else
+      ''
+    end
   end
 end
