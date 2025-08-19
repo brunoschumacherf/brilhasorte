590a9d96b59603823589f11e4b86397dd5d9ba44
Bruno Schumacher <brunoschumacherfsouza@gmail.com>
2025-08-19 01:02:56 -0300

    config cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,11 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
+  config.action_cable.allowed_request_origins = [
+    "https://brilhasorte-frontend.vercel.app",
+    "http://brilhasorte-frontend.vercel.app"
+  ]
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
