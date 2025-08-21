5f8580ef5a5fec78fe50d2750c1dbb416df92c35
Bruno Schumacher <brunoschumacherfsouza@gmail.com>
2025-08-21 20:59:37 -0300

    Fix: config cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
     "http://brilhasorte-frontend.vercel.app"
   ]
 
+  # adicionar env
+  config.action_cable.url = "wss://brilhasorte.onrender.com/cable"
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
